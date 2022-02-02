7a22fc92af31290781a024fa795de8279335b398
Esteban LECLET <estebanleclet@gmail.com>
2022-02-02 17:07:40 +0100

    sadd

--- src/js/client.js
+++ src/js/client.js
@@ -1,34 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryStack } from 'victory';
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryAxis,
+  VictoryTheme,
+  VictoryStack,
+  VictoryZoomContainer,
+} from "victory";
 
-const data2012 = [
-  {quarter: 1, earnings: 13000},
-  {quarter: 2, earnings: 16500},
-  {quarter: 3, earnings: 14250},
-  {quarter: 4, earnings: 19000}
-];
+const data2012 = [{ quarter: 1, earnings: 13000 }];
 
-const data2013 = [
-  {quarter: 1, earnings: 15000},
-  {quarter: 2, earnings: 12500},
-  {quarter: 3, earnings: 19500},
-  {quarter: 4, earnings: 13000}
-];
+let i = 0;
 
-const data2014 = [
-  {quarter: 1, earnings: 11500},
-  {quarter: 2, earnings: 13250},
-  {quarter: 3, earnings: 20000},
-  {quarter: 4, earnings: 15500}
-];
+while (i < 600) {
+  data2012.push({ quarter: i, earnings: i });
+  i++;
+}
 
-const data2015 = [
-  {quarter: 1, earnings: 18000},
-  {quarter: 2, earnings: 13250},
-  {quarter: 3, earnings: 15000},
-  {quarter: 4, earnings: 12000}
-];
+// const data2013 = [
+//   {quarter: 1, earnings: 15000},
+//   {quarter: 2, earnings: 12500},
+//   {quarter: 3, earnings: 19500},
+//   {quarter: 4, earnings: 13000}
+// ];
+
+// const data2014 = [
+//   {quarter: 1, earnings: 11500},
+//   {quarter: 2, earnings: 13250},
+//   {quarter: 3, earnings: 20000},
+//   {quarter: 4, earnings: 15500}
+// ];
+
+// const data2015 = [
+//   {quarter: 1, earnings: 18000},
+//   {quarter: 2, earnings: 13250},
+//   {quarter: 3, earnings: 15000},
+//   {quarter: 4, earnings: 12000}
+// ];
 
 class Main extends React.Component {
   render() {
@@ -36,45 +45,24 @@ class Main extends React.Component {
       <div>
         <h1>Victory Tutorial</h1>
         <VictoryChart
-          domainPadding={10}
           theme={VictoryTheme.material}
+          domain={{ x: [0, 800] }}
+          containerComponent={<VictoryZoomContainer />}
         >
-          <VictoryAxis
-            tickValues={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+          <VictoryAxis tickFormat={(x) => `${x}`} />
+          <VictoryAxis dependentAxis tickFormat={(x) => `$${x / 1000}k`} />
+
+          <VictoryBar
+            barRatio={0.8}
+            data={data2012}
+            x={"quarter"}
+            y={"earnings"}
           />
-          <VictoryAxis
-            dependentAxis
-            tickFormat={(x) => (`$${x / 1000}k`)}
-          />
-          <VictoryStack
-            colorScale={"warm"}
-          >
-            <VictoryBar
-              data={data2012}
-              x={"quarter"}
-              y={"earnings"}
-            />
-            <VictoryBar
-              data={data2013}
-              x={"quarter"}
-              y={"earnings"}
-            />
-            <VictoryBar
-              data={data2014}
-              x={"quarter"}
-              y={"earnings"}
-            />
-            <VictoryBar
-              data={data2015}
-              x={"quarter"}
-              y={"earnings"}
-            />
-          </VictoryStack>
         </VictoryChart>
       </div>
     );
   }
 }
 
-const app = document.getElementById('app');
+const app = document.getElementById("app");
 ReactDOM.render(<Main />, app);
